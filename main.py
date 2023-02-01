caca8632523c02fa2e2b76cd03df3abef20a4cb1
HarryAndrus <56275662+HarryAndrus@users.noreply.github.com>
2023-02-01 15:02:17 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-def main():
-   ##################################################
-   # Comlete your code here
-   ##################################################
+# Joshua May
+# 2/01/23
 
-    pass
+original_price = int(input('What is the regular price? '))
+rate = int(input('What is the percentage discount of the item? ')) / 100
+discount_amount = rate * original_price
+final_amount = original_price - discount_amount
+print('Regular Price: ','$', original_price)
+print('Discount Amount: ','$', int(discount_amount))
+print('The Final Price: ','$', int(final_amount))
 
-
-if __name__ == '__main__':
-    main()
+ 
